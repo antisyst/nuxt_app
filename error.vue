7e35be2014507ee5a340194427d4bf9329527e8d
Ramazan Azimli <80627823+antisyst@users.noreply.github.com>
2023-11-09 14:14:38 +0400

    Add files via upload

---
+++ error.vue
@@ -0,0 +1,16 @@
+<script>
+import Calculator from './src/components/Calculator.vue';
+import Swiper from './src/components/Swiper.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Swiper,
+    Calculator,
+  },
+};
+</script>
+<template>
+ <Swiper/>
+ <Calculator/>
+</template>
