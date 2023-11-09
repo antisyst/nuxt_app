af04d5d7fbe420b9153b2cb95fdc18ef9874165f
Ramazan Azimli <80627823+antisyst@users.noreply.github.com>
2023-11-09 14:23:58 +0400

    Add files via upload

---
+++ src/components/error.vue
@@ -0,0 +1,17 @@
+<script>
+import Calculator from './src/components/Calculator.vue';
+import Swiper from './src/components/Swiper.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Swiper,
+    Calculator,
+  },
+  props: ['error']
+}
+</script>
+<template>
+ <Swiper/>
+ <Calculator/>
+</template>
